55555fbbb611d3316fd57aff4fec4d1b08d43988
EivMeyer <eiv.meyer@gmail.com>
2024-11-27 16:57:01 +0100

    Fix bug in delayed func execution helper

--- commonroad_geometric/common/threading/delayed_executor.py
+++ commonroad_geometric/common/threading/delayed_executor.py
@@ -46,7 +46,7 @@ class DelayedExecutor(AutoReprMixin):
         if not self._enabled or self._delay == 0.0:
             return fn(*args, **kwargs)
 
-        logger.debug(f"Created delay-injected version of {fn.__module__}:{fn.__name__}")
+        logger.debug(f"Created delay-injected version of {fn.__name__}")
         delayed_fn = self.inject_delays(fn=fn)
         result = delayed_fn(*args, **kwargs)
         return result
@@ -92,60 +92,47 @@ class DelayedExecutor(AutoReprMixin):
     ) -> AnyCallable[T_AnyReturn]:
         """
         Creates a new function with injected delays.
-
-        Simple example as follows:
-
-        .. code-block:: python
-
-        def input_fn():
-            print(1)
-            print(2)
-            print(3)
-
-        def output_fn(): # inject_delays(input_fn)
-            _Sleeper()
-            print(1)
-            _Sleeper()
-            print(2)
-            _Sleeper()
-            print(3)
         """
+        # Check if fn is a function or method with accessible source code
+        try:
+            source_code = inspect.getsourcelines(fn)[0]
+        except (TypeError, OSError) as e:
+            logger.debug(f"Cannot inject delays into {fn}: {e}")
+            return fn  # Return the original function without modification
 
-        # extracting the raw source code of the input function
-        source_code = inspect.getsourcelines(fn)[0]
+        # Proceed with delay injection as before
         sleep_statement = '_Sleeper()'
 
-        # extracting the length (LOC) of the function header
-        # TODO: this is very hacky, should be done via inspect
+        # Extract the length (LOC) of the function header
         header_end_line_idx = next((i for i in range(len(source_code))
-                                   if source_code[i].rstrip('\n').rstrip(' ').endswith(':')))
+                                    if source_code[i].rstrip('\n').rstrip(' ').endswith(':')), None)
+        if header_end_line_idx is None:
+            logger.warning(f"Cannot find function header in {fn}")
+            return fn
 
-        # merging the LOC to a single-line function header (can originally be split over multiple lines)
-        # TODO: this is very hacky, should be done via inspect
+        # Merge the LOC to a single-line function header
         delayed_fn_name = f"delayed_{fn.__name__}"
         header = ''.join([source_code[i].rstrip('\n').lstrip()
-                         for i in range(header_end_line_idx + 1) if len(source_code[i].strip('\n')) > 0])
+                        for i in range(header_end_line_idx + 1) if len(source_code[i].strip('\n')) > 0])
 
         base_indent = get_indent(source_code[0])
 
-        # creating new function params, injecting the _Sleeper as a function parameter
+        # Create new function params, injecting the _Sleeper as a function parameter
         params = ['_Sleeper'] + [k for k in inspect.signature(fn).parameters.keys()]
         if inspect.ismethod(fn):
             params = ['self'] + params
 
-        # initializing the delay-injected code (one list item = one line of code)
-        # TODO: we should not use str.replace here at all, as it can fail for some edge cases
+        # Initialize the delay-injected code
         delayed_code: List[str] = [
             lstrip_n(header.replace(fn.__name__, delayed_fn_name, 1).replace('self', 'self, _Sleeper'), base_indent),
         ]
 
-        # keeping track of current indentation level (number of spaces)
+        # Keep track of current indentation level
         prev_indent = base_indent
         skipping = False
         for line_idx in range(header_end_line_idx + 1, len(source_code)):
             line = source_code[line_idx].rstrip('\n')
             skip_next = skipping
-            # we can't insert sleep statements in the middle of a function call, so we skip those
             if line.endswith('('):
                 skip_next = True
             elif line.endswith(')'):
@@ -153,31 +140,29 @@ class DelayedExecutor(AutoReprMixin):
             stripped_line = line.strip()
             if not skipping and len(line.strip()) > 0 and stripped_line[0] != '#':
                 this_indent = get_indent(line)
-                # calculating the appropriate indentation for the sleep statement to avoid syntax errors
                 sleep_indent = max(prev_indent, this_indent)
-                # here we insert the sleep statement
                 sleep_statement_line = lstrip_n(' ' * sleep_indent + sleep_statement, base_indent) + '\n'
                 delayed_code.append(sleep_statement_line)
                 prev_indent = this_indent
             delayed_code.append(lstrip_n(line, base_indent) + '\n')
             skipping = skip_next
 
-        # compiling new source code as string
+        # Compile new source code as string
         delayed_source_code = '\n'.join(delayed_code)
 
-        # updating context so that the exec call won't lack any references
+        # Update context for exec call
         locals().update(inspect.getmodule(fn).__dict__)
         globals().update(fn.__globals__)
 
-        # creating the new function
+        # Create the new function
         exec(delayed_source_code)
 
-        # obtaining the function that was created
+        # Obtain the function that was created
         delayed_fn_ = locals().get(delayed_fn_name)
 
-        # creating a wrapper function that injects the _Sleeper
+        # Create a wrapper function that injects the _Sleeper
         if inspect.ismethod(fn):
-            # for methods, we also inject the self argument
+            # For methods, we also inject the self argument
             def delayed_fn_wrapper(*args: Any, **kwargs: Any) -> Any:
                 return delayed_fn_(fn.__self__, self._sleeper, *args, **kwargs)
         else:
@@ -185,7 +170,7 @@ class DelayedExecutor(AutoReprMixin):
                 return delayed_fn_(self._sleeper, *args, **kwargs)
         delayed_fn = delayed_fn_wrapper
 
-        # updating some metadata for the new function
+        # Update metadata for the new function
         delayed_fn.__dir__ = fn.__dir__
         delayed_fn.__name__ = fn.__name__
         delayed_fn.__qualname__ = fn.__qualname__
